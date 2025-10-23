5d4d0cef78990f44e9113dce1662cb86682b30d3
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2025-10-23 09:44:43 +0300

    HomeView style update

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { useRouter } from 'vue-router'
 
@@ -13,43 +10,47 @@ const logout = () => {
   router.push('/')
 }
 
-
 const goToLobby = () => navigateTo('/lobby')
 
 const navigateTo = (route) => {
   router.push(route);
 };
-
-const username = ref('')
 </script>
 
 <template>
-  <div class="flex h-screen w-screen items-center justify-center bg-muted">
-    <Card class="w-[350px] shadow-lg">
-      <CardHeader>
-        <CardTitle class="text-center text-2xl font-bold">
-          Scotland Yard
-        </CardTitle>
-      </CardHeader>
-      <CardContent class="space-y-4">
-        <div class="space-y-2">
-          <Label for="username">Username</Label>
-          <Input
-            id="username"
-            v-model="username"
-            placeholder="Enter your name"
-          />
-        </div>
+  <div class="flex flex-col h-screen w-screen items-center justify-center bg-[url('https://cdn-imgix.headout.com/media/images/f41bdd0b95a2fc1fa8aff71725ffe5e3-2963-london-evening-cruise-on-river-thames-04.JPG')] bg-cover bg-center bg-black/50 bg-blend-darken">
+    
+    <div class="flex justify-center items-center">
+      <div class="bg-[url(./assets/image.webp)] bg-size-[360px] bg-no-repeat bg-center h-56 w-96">
+      </div>
+    </div>
 
+    <Card class="relative w-[420px] border-2 border-slate-200 bg-white/95 backdrop-blur-sm shadow-2xl">
+      
+      <CardContent class="space-y-4">
         <div class="flex flex-col gap-3">
-          <Button @click="goToLobby" class="w-full" variant="default">
+          <Button 
+            @click="goToLobby" 
+            class="w-full py-6 shadow-xl" 
+            variant="default"
+          >
             Create Lobby
           </Button>
-          <Button @click="goToLobby" class="w-full" variant="secondary">
+          
+          <Button 
+            @click="goToLobby" 
+            class="w-full py-6 shadow-xl" 
+            variant="secondary"
+          >
             Join Lobby
           </Button>
-          <Button @click="logout" variant="destructive">
-            logout
+          
+          <Button 
+            @click="logout" 
+            class="w-full py-6 shadow-xl" 
+            variant="destructive"
+          >
+          Logout
           </Button>
         </div>
       </CardContent>
